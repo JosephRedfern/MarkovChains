205a16a46abdb66244654db4119dc78399169b86
Joseph Redfern <joseph@redfern.me>
2014-11-18 13:05:51 +0000

    Initial Commit

---
+++ Markov/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Markov
+//
+//  Created by Joseph Redfern on 18/11/2014.
+//  Copyright (c) 2014 Joseph Redfern. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
